2b298191796155a729b43a1cc280a2a3ca36a612
KiKaily <noelcresencio@gmail.com>
2025-02-11 17:21:03 +0100

    s

--- src/pages/artwork/Ceramics.tsx
+++ src/pages/artwork/Ceramics.tsx
@@ -17,8 +17,52 @@ const Ceramics = () => {
               <p className="text-muted-foreground">
               Exposición realizada en Denia en enero de 2023.
               </p>
-
+              <div className="grid md:grid-cols-3 gap-4">
+                <img
+                  src="/kai07.jpg"
+                  alt="Naturaleza 1"
+                  className="w-full h-full object-cover rounded-lg"
+                />
+                <img
+                  src="/kai08.jpg"
+                  alt="Naturaleza 2"
+                  className="w-full h-full object-cover rounded-lg"
+                />
+                <img
+                  src="/kai09.jpg"
+                  alt="Naturaleza 2"
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
               <div className="my-4"></div>
+
+              
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardContent className="p-6 space-y-4">
+              <h2 className="text-2xl font-semibold">KAIRÓS</h2>
+              <h3 className="text-xl text-muted-foreground">Exposición en Jávea de febrero a marzo de 2023</h3>
+              <p className="text-muted-foreground">
+              “Hay un concepto en griego para el momento crítico, especial, que puede cambiar las cosas: kairós. Expresa el sentimiento de que cualquier crisis es ambivalente y fácilmente puede inclinarse tanto a la fatalidad como a la fortuna. Es una intuición de la fragilidad humana, de las encrucijadas que esconden su signo”.
+              </p>
+              <p className="text-muted-foreground">
+              Alejandro Gándara. Dioses contra microbios, 2020.              </p>
+              <p className="text-muted-foreground">
+              “Este es un viaje pegado a la tierra, que agarra el barro para agarrarse a la vida.              </p>
+              <p className="text-muted-foreground">
+              En el principio, el silencio. Una esfera suspendida en el aire marca el tiempo como un péndulo en la mano de Dios. Piezas de barro refractario, horadadas y cosidas con cuerda, recuerdan a las celosías de los monasterios, los hábitos de los monjes, el recogimiento en la penumbra. Sunt lacrimae rerum (Virgilio. La Eneida, siglo I a.C.). Hay lágrimas en las cosas, contienen el aliento de quien las creó, el tiempo que dedicó a pensarlas y a darles forma.              </p>
+              <p className="text-muted-foreground">
+              El sonido llega a través del mar. Con él llegan también conchas, piedras y residuos plásticos. Y la luz del Mediterráneo. Barro porcelánico, suave, del color de la sal. Y el jolgorio de la gente, la vida ahí fuera. Flores, telas de colores, campanas de cristal que protegen, pero dejan ver lo que hay dentro. La piedra se transforma en seres intermedios que abren la boca y nos desafían. Detente. Escucha.              </p>
+              <p className="text-muted-foreground">
+              Este edificio tan hermoso alberga una exposición y algo más. Una intuición, una oportunidad.”              </p>
+              <p className="text-muted-foreground">
+              Jana García González. Febrero, 2023              </p>
+
+              <div className="my-10 bg-red-500"> {/* Añadido un fondo rojo para visualizar el div */}
+                                                 {/* Este div debería tener un margen vertical de 2.5rem (40px) */}
+              </div>
 
               <div className="grid md:grid-cols-3 gap-4">
                 <img
@@ -56,45 +100,6 @@ const Ceramics = () => {
               <p className="text-muted-foreground">
               *Las primeras 5 fotos hechas por ©Daniela Burtsch. dbutsch@ariadnefilme.de www.danielabutsch.eu
               </p>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardContent className="p-6 space-y-4">
-              <h2 className="text-2xl font-semibold">KAIRÓS</h2>
-              <h3 className="text-xl text-muted-foreground">Exposición en Jávea de febrero a marzo de 2023</h3>
-              <p className="text-muted-foreground">
-              “Hay un concepto en griego para el momento crítico, especial, que puede cambiar las cosas: kairós. Expresa el sentimiento de que cualquier crisis es ambivalente y fácilmente puede inclinarse tanto a la fatalidad como a la fortuna. Es una intuición de la fragilidad humana, de las encrucijadas que esconden su signo”.
-              </p>
-              <p className="text-muted-foreground">
-              Alejandro Gándara. Dioses contra microbios, 2020.              </p>
-              <p className="text-muted-foreground">
-              “Este es un viaje pegado a la tierra, que agarra el barro para agarrarse a la vida.              </p>
-              <p className="text-muted-foreground">
-              En el principio, el silencio. Una esfera suspendida en el aire marca el tiempo como un péndulo en la mano de Dios. Piezas de barro refractario, horadadas y cosidas con cuerda, recuerdan a las celosías de los monasterios, los hábitos de los monjes, el recogimiento en la penumbra. Sunt lacrimae rerum (Virgilio. La Eneida, siglo I a.C.). Hay lágrimas en las cosas, contienen el aliento de quien las creó, el tiempo que dedicó a pensarlas y a darles forma.              </p>
-              <p className="text-muted-foreground">
-              El sonido llega a través del mar. Con él llegan también conchas, piedras y residuos plásticos. Y la luz del Mediterráneo. Barro porcelánico, suave, del color de la sal. Y el jolgorio de la gente, la vida ahí fuera. Flores, telas de colores, campanas de cristal que protegen, pero dejan ver lo que hay dentro. La piedra se transforma en seres intermedios que abren la boca y nos desafían. Detente. Escucha.              </p>
-              <p className="text-muted-foreground">
-              Este edificio tan hermoso alberga una exposición y algo más. Una intuición, una oportunidad.”              </p>
-              <p className="text-muted-foreground">
-              Jana García González. Febrero, 2023              </p>
-
-              <div className="my-10 bg-red-500"> {/* Añadido un fondo rojo para visualizar el div */}
-                                                 {/* Este div debería tener un margen vertical de 2.5rem (40px) */}
-              </div>
-
-              <div className="grid md:grid-cols-2 gap-4">
-                <img
-                  src="/kai07.jpg"
-                  alt="Naturaleza 1"
-                  className="w-full h-full object-cover rounded-lg"
-                />
-                <img
-                  src="/kai09.jpg"
-                  alt="Naturaleza 2"
-                  className="w-full h-full object-cover rounded-lg"
-                />
-              </div>
               <section className="relative w-full h-[60vh] overflow-hidden">
               <video
               autoPlay
